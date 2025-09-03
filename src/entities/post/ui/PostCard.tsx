f07d40a0a8a5485beb320fef19d784c13c351036
shadowic <shadowic.ya@gmail.com>
2025-09-04 00:23:35 +0300

    fix: post card styles

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -14,8 +14,8 @@ interface PostCardProps {
 export const PostCard = ({ post }: PostCardProps) => {
   return (
     <div className={styles.card}>
-      <h3>{post.title}</h3>
-      <p>{post.body}</p>
+      <h3 className={styles.title}>{post.title}</h3>
+      <p className={styles.text}>{post.body}</p>
       <small className={styles.caption}>
         User ID: {post.userId} | Post ID: {post.id}
       </small>
